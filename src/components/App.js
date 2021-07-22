407c1f279e1bdc5ae83cb0137beadc14b462a196
george-shammar <george.gbenle@outlook.com>
2021-07-22 17:26:47 +0000

    Add form with drop down

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-// import BooksForm from '../containers/BooksForm';
+import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
 
 const App = () => {
     return (
         <>
-            <BooksList />
-            {/* <BooksForm /> */}
+         <BooksForm />
+        <BooksList />
+           
         </>
     );
 }
--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const BookForm = () => {
     return (
-        <div></div>
+        <div>
+            <input type="text" placeholder="Title"></input>
+             <label>Category:</label>
+                <select>
+                    <option value="Action">Action</option>
+                    <option value="Biography">Biography</option>
+                    <option value="History">History</option>
+                    <option value="Horror">Horror</option>
+                    <option value="Kids">Kids</option>
+                    <option value="Learning">Learning</option>
+                    <option value="Sci-Fi">Sci-Fi</option>
+                </select>
+            <button>Submit</button>
+        </div>
     )
 }
 
