f083aec4818413e543d930faac854d73f21c79c9
george-shammar <george.gbenle@outlook.com>
2021-07-22 21:05:31 +0000

    Import provider from react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux'
 
 //initial state
 const initialState = {
@@ -35,6 +36,7 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+export default initialState;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,3 +1,4 @@
+import initialState from "../index";
  //reducers
 
 // booksReducer
