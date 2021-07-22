cae3e6131647f93807a13672a8cae06f474e7a59
george-shammar <george.gbenle@outlook.com>
2021-07-22 18:50:31 +0000

    Implement createBook action reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -2,5 +2,22 @@
 
 //initial state
 const initialState = {
-    allBooks: {},
+    allBooks: []
 };
+
+// booksReducer
+
+const booksReducer = (state = initialState, action) => {
+    switch(action.type) {
+      case 'allBooks/createBook':
+        return {
+          ...state, 
+          allBooks: [...state.allBooks, action.payload]
+      };
+
+      
+
+      default:
+        return state;
+    }
+}
