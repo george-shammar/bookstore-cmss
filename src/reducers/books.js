44adb2413f4c03a821fc1f2498fdfde34a41e13a
george-shammar <george.gbenle@outlook.com>
2021-07-22 18:53:45 +0000

    Implement removeBook action reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -15,7 +15,14 @@ const booksReducer = (state = initialState, action) => {
           allBooks: [...state.allBooks, action.payload]
       };
 
-      
+      case 'allBooks/removeBook':
+      return {
+        ...state,
+        allBooks: state.allBooks.filter(allBooks => {
+          return (allBooks.id !== action.payload.id)
+          allBooks;
+        })
+      }
 
       default:
         return state;
