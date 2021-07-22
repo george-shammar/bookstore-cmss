1062c3d54f9ac91cc58f42eefa47b3a4e7cb5fc9
george-shammar <george.gbenle@outlook.com>
2021-07-22 18:18:09 +0000

    Implement createBook action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,16 @@
 
 const createBook = (book) => {
     return {
-      type: 'allBooks/addBook',
+      type: 'allBooks/createBook',
       payload: book
     }
-  }
+}
+
+// remove books action
+
+const removeBook = (book) => {
+    return {
+      type: 'allBooks/removeBook',
+      payload: book
+    }
+}
