9a19c6aee9aa6d6b70b5784e04e05a13f6fee126
george-shammar <george.gbenle@outlook.com>
2021-07-22 23:38:55 +0000

    Import useSelector from react-redux

--- src/index.js
+++ src/index.js
@@ -37,7 +37,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+export const selectAllBooks = (state) => state.allBooks;
+
 export default initialState;
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
