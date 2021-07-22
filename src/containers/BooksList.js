72e64502fd806060eeb6e0e9ec147d3a44745dc4
george-shammar <george.gbenle@outlook.com>
2021-07-22 13:11:35 +0000

    Resolve dependency errors

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const BooksList = () => {
     return (
-        <div></div>
+        <div>
+             <table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table> 
+        </div>
     )
 }
 
