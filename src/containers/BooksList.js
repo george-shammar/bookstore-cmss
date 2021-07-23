4e8c6daf48fcf4726fc86224bc9301a7b6cf993f
george-shammar <george.gbenle@outlook.com>
2021-07-23 16:42:04 +0000

    Update Booklist

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectAllBooks } from '../index';
+import { initialState } from '../reducers/books';
+
+const selectAllBooks = (state) => initialState.allBooks[0];
 
 const BooksList = () => {
-    return (
-      <div>
-        <table>
-          <tr>
-            <th>Book Id</th>
-            <th>Title</th>
-            <th>Category</th>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </table> 
-      </div>
-    )
+  const allBooks = useSelector(selectAllBooks);
+  console.log(allBooks);
+  return (
+    <div></div>
+  );
+
+
+
+    // return (
+    //   <div>
+    //     <table>
+    //       <tr>
+    //         <th>Book Id</th>
+    //         <th>Title</th>
+    //         <th>Category</th>
+    //       </tr>
+    //       <tr>
+    //         <td></td>
+    //         <td></td>
+    //         <td></td>
+    //       </tr>
+    //       <tr>
+    //         <td></td>
+    //         <td></td>
+    //         <td></td>
+    //       </tr>
+    //     </table> 
+    //   </div>
+    // )
 }
 
 export default BooksList;
