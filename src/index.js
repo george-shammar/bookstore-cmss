7851f561119ee45d6bab0e0f8e226f9cbab55c28
george-shammar <george.gbenle@outlook.com>
2021-07-23 16:43:33 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store } from '../src/reducers/books';
 
 //initial state
-const initialState = {
-  allBooks: [
-    { 
-      bookId: Math.floor((Math.random() * 100) + 1),
-      title: "The Hermit",
-      category: "History"
-    }, 
-    {
-      bookId: Math.floor((Math.random() * 100) + 1),
-      title: "Queen of Endol",
-      category: "Horror"
-    },
-    {
-      bookId: Math.floor((Math.random() * 100) + 1),
-      title: "Goals",
-      category: "Learning"
-    },
-    {
-      bookId: Math.floor((Math.random() * 100) + 1),
-      title: "Helena of Avalor",
-      category: "Kids"
-    }
-  ]
-};
+// const initialState = {
+//   allBooks: [
+//     { 
+//       bookId: Math.floor((Math.random() * 100) + 1),
+//       title: "The Hermit",
+//       category: "History"
+//     }, 
+//     {
+//       bookId: Math.floor((Math.random() * 100) + 1),
+//       title: "Queen of Endol",
+//       category: "Horror"
+//     },
+//     {
+//       bookId: Math.floor((Math.random() * 100) + 1),
+//       title: "Goals",
+//       category: "Learning"
+//     },
+//     {
+//       bookId: Math.floor((Math.random() * 100) + 1),
+//       title: "Helena of Avalor",
+//       category: "Kids"
+//     }
+//   ]
+// };
 
 ReactDOM.render(
   <Provider store={store}>
@@ -37,6 +37,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-const selectAllBooks = (state) => state.allBooks;
-
-export { initialState, selectAllBooks };
