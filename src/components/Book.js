c448e61324cb7ad827e1f693e848e53444c9b642
george-shammar <george.gbenle@outlook.com>
2021-07-29 23:19:56 +0000

    Refactor Book container

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,41 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-// import { initialState } from '../reducers/books';
-// import store from '../reducers/index';
-import { selectAllBooks } from '../reducers/books';
-
-// const selectAllBooks = () => initialState.allBooks;
 
 const Book = (props) => {
-  const { handleRemove } = props;
-
-  const allBooks = useSelector(selectAllBooks);
+  const { book, handleRemoveBook } = props;
 
   return (
-    <div>
-      <table>
-        <tbody>
-          {allBooks.map((book) => (
-            <tr key={book.id}>
-              <td>{book.bookId}</td>
-              <td>{book.title}</td>
-              <td>{book.category}</td>
-              <td><button type="submit" onClick={handleRemove}>Remove Book</button></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <tr>
+      <td>{book.id}</td>
+      <td>{book.title}</td>
+      <td>{book.category}</td>
+      <td><button type="button" name="button" value={book} onClick={() => handleRemoveBook(book)}>Remove Book</button></td>
+    </tr>
   );
 };
 
-Book.propTypes = {
-  handleRemove: PropTypes.func,
-};
-
-Book.defaultProps = {
-  handleRemove: null,
-};
+Book.propTypes = { book: PropTypes.instanceOf(Object).isRequired };
+Book.propTypes = { handleRemoveBook: PropTypes.func.isRequired };
 
 export default Book;
