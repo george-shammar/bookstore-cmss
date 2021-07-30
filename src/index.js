16a85e4cf4eeab0309ac63c513a66531e9eefabc
george-shammar <george.gbenle@outlook.com>
2021-07-30 01:13:13 +0000

    Update ReactDom.render

--- src/index.js
+++ src/index.js
@@ -41,10 +41,8 @@ const initialState = [
 const store = createStore(rootReducer, { books: initialState });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
