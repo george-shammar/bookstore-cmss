ac728cc9815cefff3950baa51a3fad187746be43
george-shammar <george.gbenle@outlook.com>
2021-07-30 16:36:02 +0000

    Add margin to header elements

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import '../stylesheets/App.css';
 const App = () => (
   <>
     <div className="container mt-5 border">
-      <div className="header border py-2 px-5">
+      <div className="header border py-2 px-5 mb-5">
         <div className="left-header">
           <h1 className="blue">Bookstore CMS</h1>
           <p className="mt-3">BOOKS</p>
