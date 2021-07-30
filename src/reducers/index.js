d73ffafdab1df047198abc7dbe993eed58f1d64a
george-shammar <george.gbenle@outlook.com>
2021-07-30 11:32:35 +0000

    Import filter reducer into combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from './books';
+import filterReducer from './filter';
 
 const reducers = {
   books: bookReducer,
+  filter: filterReducer,
 };
 
 const rootReducer = combineReducers(reducers);
