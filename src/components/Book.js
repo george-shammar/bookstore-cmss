08ca5bacfbb0ba72b6272f963dcb5070b310c73a
george-shammar <george.gbenle@outlook.com>
2021-07-30 18:26:17 +0000

    Style header

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,13 +6,18 @@ const Book = (props) => {
   const { book, handleRemove } = props;
 
   return (
-    <div>
-      <p className="font">{book.category}</p>
-      <h4 className="fw-bold font">{book.title}</h4>
-      <div className="blue d-flex">
-        <button type="button" className="text">Comments</button>
-        <button type="button" className="text" value={book} onClick={() => handleRemove(book)}>Remove</button>
-        <button type="button" className="text">Edit</button>
+    <div className="px-5 ">
+      <div className="border py-4 px-3 mb-3">
+        <div className="card-title">
+          <p className="font s-size">{book.category}</p>
+          <h6 className="fw-bold font title">{book.title}</h6>
+          <p className="s-size blue">George Shammar</p>
+        </div>
+        <div className="blue d-flex">
+          <button type="button" className="text font s-size">Comments</button>
+          <button type="button" className="text font s-size" value={book} onClick={() => handleRemove(book)}>Remove</button>
+          <button type="button" className="text font s-size">Edit</button>
+        </div>
       </div>
     </div>
   );
