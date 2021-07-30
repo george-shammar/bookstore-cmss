7c9d21cb939ba72abad5d86b65e46439e2a43d78
george-shammar <george.gbenle@outlook.com>
2021-07-30 13:43:44 +0000

    Remove card margin

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import '../stylesheets/App.css';
 const App = () => (
   <>
     <div className="container mt-5 border">
-      <div className="header">
+      <div className="header border py-2 px-5">
         <div className="left-header">
           <h1>Bookstore CMS</h1>
           <p>BOOKS</p>
