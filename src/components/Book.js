36b6ab33c48dadb6c9bcea73f571b1b247cdecd9
george-shammar <george.gbenle@outlook.com>
2021-07-30 18:57:48 +0000

    Style book card

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,16 +7,16 @@ const Book = (props) => {
 
   return (
     <div className="px-5 ">
-      <div className="border py-4 px-3 mb-3 d-flex">
+      <div className="border py-4 px-3 mb-3 card">
         <div className="card-title">
           <p className="font s-size">{book.category}</p>
           <h6 className="fw-bold font title">{book.title}</h6>
           <p className="s-size blue">George Shammar</p>
-        </div>
-        <div className="blue d-flex">
-          <button type="button" className="text font s-size">Comments</button>
-          <button type="button" className="text font s-size" value={book} onClick={() => handleRemove(book)}>Remove</button>
-          <button type="button" className="text font s-size">Edit</button>
+          <div className="blue d-flex">
+            <button type="button" className="text font s-size">Comments</button>
+            <button type="button" className="text font s-size" value={book} onClick={() => handleRemove(book)}>Remove</button>
+            <button type="button" className="text font s-size">Edit</button>
+          </div>
         </div>
         <div className="completed d-flex">
           <div className="circle" />
